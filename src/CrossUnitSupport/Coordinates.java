4e5ca60049a7e0c64c206655f09c69efc33c6f84
Alex Nekhaev <alexnekhaev@gmail.com>
2018-05-15 19:51:20 +0300

    Coordinates method "setX" and "setY" added.

--- src/CrossUnitSupport/Coordinates.java
+++ src/CrossUnitSupport/Coordinates.java
@@ -12,6 +12,14 @@ public class Coordinates {
         return y;
     }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     public Coordinates getXY() {
         return new Coordinates(x, y);
     }
