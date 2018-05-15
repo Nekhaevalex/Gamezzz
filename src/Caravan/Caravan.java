4f42496eaf13f3b31d42e6257ad460818d8460bb
Alex Nekhaev <alexnekhaev@gmail.com>
2018-05-15 18:28:22 +0300

    Minor improves

--- src/Caravan/Caravan.java
+++ src/Caravan/Caravan.java
@@ -5,6 +5,7 @@ import game.IWorld;
 
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
 
 //TODO: Караван, – время жизни: infinity, – повозка: желтая, 10x20px, – берет и увозит с собой, сбрасывает в случайном месте,– скорость 20
 
@@ -17,6 +18,8 @@ public class Caravan implements IUnit {
     private int vx;
     private int vy;
 
+    private ArrayList<IUnit> inventory;
+
     @Override
     public void draw(Graphics2D canvas) {
         canvas.setColor(new Color(255, 195,0));
@@ -27,16 +30,23 @@ public class Caravan implements IUnit {
         canvas.drawOval(x+5,y+1,4,4);
         canvas.fillOval(x-10,y+1,4,4);
         canvas.fillOval(x+5,y+1,4,4);
+    }
+
+    private void compressUnit(IUnit unit) {
+        inventory.add(unit);
 
     }
 
     @Override
     public void step(IWorld world, float dt) {
+        int ww = world.getWidth();
+        int wh = world.getHeight();
 
     }
 
     public Caravan(int x, int y) {
         this.x = x;
         this.y = y;
+        inventory = new ArrayList<>();
     }
 }
