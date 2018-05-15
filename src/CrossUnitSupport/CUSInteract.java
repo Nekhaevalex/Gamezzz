d185322f8e59885aedaf010ca82f816960843da2
Alex Nekhaev <alexnekhaev@gmail.com>
2018-05-15 20:40:31 +0300

    Clone method Override (hope it will help with cloning)

--- src/CrossUnitSupport/CUSInteract.java
+++ src/CrossUnitSupport/CUSInteract.java
@@ -1,7 +1,12 @@
 package CrossUnitSupport;
 
 
-abstract public class CUSInteract {
+abstract public class CUSInteract implements Cloneable{
     public abstract Coordinates getXY();
     public abstract void setXY(Coordinates position);
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
